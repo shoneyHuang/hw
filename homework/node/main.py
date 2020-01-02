532d0cd146c6b1021e980bd544bcd90853931c79
ShoneyHuang <31814059+shoneyHuang@users.noreply.github.com>
2020-01-02 15:26:18 +0800

    update by esun

--- homework/node/main.py
+++ homework/node/main.py
@@ -1,24 +1,3 @@
-from hw1 import Locomotive,PassengerCar,FuelCar,CarType
-from hw6 import Train,Car
+from userinterface import UserInterface
 
-nodecar = Train()
-
-locomotive = Locomotive('A Manufactor',CarType.LOCOMOTIVE,'shoney huang',10)
-
-nodecar.createTrain(locomotive)
-nodecar.showHead()
-nodecar.traversal()
-print(nodecar.getCarCount())
-
-# car = []
-# car.append(FuelCar('B Manufactor',CarType.FUEL,100))
-# car.append(FuelCar('B Manufactor',CarType.FUEL,150))
-# car.append(FuelCar('A Manufactor',CarType.FUEL,200))
-# car.append(PassengerCar('C Manufactor',CarType.PASSENGER,10))
-# car.append(PassengerCar('A Manufactor',CarType.PASSENGER,15))
-# car.append(PassengerCar('B Manufactor',CarType.PASSENGER,7))
-# car.append(FuelCar('A Manufactor',CarType.FUEL,250))
-# car.append(FuelCar('C Manufactor',CarType.FUEL,300))
-# car.append(FuelCar('C Manufactor',CarType.FUEL,447))
-
-# x = Locomotive('A Manufactor',CarType.LOCOMOTIVE,'shoney huang',10,car)
+UserInterface()
